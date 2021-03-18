e3c04b7e32e13ddd672e52d7a85196d1f2cb4320
Sam Wu <swu@nait.ca>
2021-03-17 21:05:57 -0600

    finished implementing URL security constraints

---
+++ src/main/java/dmit2015/security/Security.java
@@ -0,0 +1,39 @@
+package dmit2015.security;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.security.enterprise.SecurityContext;
+import javax.servlet.http.HttpServletRequest;
+
+@Named
+@ApplicationScoped
+public class Security {
+
+    @Inject
+    private SecurityContext securityContext;
+
+    public boolean hasAccessToWebResource(String resource) {
+        return securityContext.hasAccessToWebResource(resource, "GET");
+    }
+
+    public static boolean isInAnyRole(HttpServletRequest request, String... roles) {
+        for (String singleRole : roles) {
+            if (request.isUserInRole(singleRole)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean isInAllRole(HttpServletRequest request, String... roles) {
+        for (String singleRole : roles) {
+            if (!request.isUserInRole(singleRole)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
+
